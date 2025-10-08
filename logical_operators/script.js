bff78ace1430395c15ec284ef1bc3745154f1831
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-10-08 20:18:05 +0530

    Commit for logical operators

---
+++ logical_operators/script.js
@@ -0,0 +1,11 @@
+// logical operators =  used to combine or manipulate Boolean values 
+//                                    (true or false)
+
+// AND = &&
+// OR  = ||
+// NOT = !
+
+
+console.log(10>5 && 5<10)
+console.log(10>5 || 15<10)
+console.log(!(10>5 && 5<10))
